e39eb5054bdd8f48e70ac49cc1738c1857ee4a7b
RahulGoy <rahulkumargoyal00@gmail.com>
2020-07-18 10:07:21 +0530

    Some issues to sort

--- Django_Project/urls.py
+++ Django_Project/urls.py
@@ -6,7 +6,6 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
@@ -14,4 +13,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('', include('blog.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
