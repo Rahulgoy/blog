06a200208653035501b7d85379ad1273a95b5c3f
RahulGoy <rahulkumargoyal00@gmail.com>
2020-07-18 23:01:35 +0530

    Register issue fixed

--- Django_Project/urls.py
+++ Django_Project/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
     path('password-reset-CONFIRM/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
     path('', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
