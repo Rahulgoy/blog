83407adbadf838a4219277d2d2b31d7dc998e813
Rahul Goyal <rahul@rahul.org>
2020-12-28 17:37:03 -0500

    Updated Admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, BlogComment
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    fields=['title','author','content','likes'] 
+    list_display=('title','author','content')
+    search_fields=['title','author']
+admin.site.register(Post,PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=('author','content')
+    search_fields=['author']
+admin.site.register(BlogComment,CommentAdmin)
